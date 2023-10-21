f87980f4c05d407903d648d902e47c7305a4576b
gilvan alves <gilvan.dev@gmail.com>
2023-10-21 09:34:16 -0300

    adicionando configurações do vite

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ document.querySelector('#app').innerHTML = `
       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
     </a>
     <h1>Hello Vite!</h1>
+    <h1>TopMagazine</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
---
+++ vite.config.js
@@ -0,0 +1,24 @@
+// import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  base: "/<nome_repositorio_github>/"
+//   plugins: [],
+//   resolve: {
+  
+//   },
+//   css: {
+//     postcss: {
+//       plugins: [tailwindcss],
+//     },
+//   },
+//   build: {
+//     rollupOptions: {
+//       input: {
+//         main: resolve(__dirname, "./index.html"),
+//         checkout: resolve(__dirname, "./checkout.html"),
+//         pedidos: resolve(__dirname, "./pedidos.html"),
+//       },
+//     },
+//   },
+});
